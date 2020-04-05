3264e87f024e0bda24f96d879711d53c82339627
Thomas <thomas.cacheux@etu.univ-valenciennes.fr>
2020-04-05 17:33:09 +0200

    inc

--- src/main/java/uphf/tnsi/tpspring/repository/OperationRepository.java
+++ src/main/java/uphf/tnsi/tpspring/repository/OperationRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface OperationRepository extends CrudRepository<Operation,Long> {
     List<Operation>  findAll();
     Operation findByIdOperation(int idOperation);
+    List<Operation> findAllByIdCustomer(Long idCustomer);
 }
---
+++ src/main/java/uphf/tnsi/tpspring/webcontroller/StandardController.java
@@ -0,0 +1,42 @@
+package uphf.tnsi.tpspring.webcontroller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import uphf.tnsi.tpspring.entity.Operation;
+import uphf.tnsi.tpspring.repository.OperationRepository;
+
+import java.util.List;
+
+@Controller
+public class StandardController {
+    @Autowired
+    OperationRepository operationRepository;
+
+    @GetMapping("/login")
+    public String login(Model model){
+        return "auth";
+    }
+
+    @RequestMapping("/loginFailed")
+    public String loginError(Model model){
+        model.addAttribute("error", "true");
+        return "auth";
+    }
+
+    @GetMapping("/compte/{id}")
+    public String compte(Model model,  @PathVariable Long id){
+        List<Operation> operations = operationRepository.findAllByIdCustomer(id);
+        model.addAttribute("operations", operations);
+        return "compteinfos";
+    }
+
+    @GetMapping("/compte")
+    public String compte(Model model){
+        return "compteinfos";
+    }
+}
